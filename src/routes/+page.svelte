3ed89e8702380318ffff19af8280a71c454cb7cd
Tristan F <leodog896@gmail.com>
2023-07-13 22:07:27 -0400

    feat: grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
     let x = 0;
     let y = 0;
 
+    let mouseDown = false;
+    let dragging = false;
+
     let boxSize = 50;
 
     let width: number;
@@ -11,26 +14,62 @@
 
     type Vector = [x: number, y: number];
 
-    const map = new Map<Vector, boolean>();
-    
+    const map: Record<string, boolean> = {};
+
+    function screenToMap(xCursor: number, yCursor: number): Vector {
+        return [
+            Math.floor((xCursor - x) / boxSize),
+            Math.floor((yCursor - y) / boxSize)
+        ]
+    }
+
     let render: Render;
     $: render = ({ context, width, height }) => {
-        context.fillStyle = 'white';
-        for (let i = 0; i < width / boxSize; i++) {
-            for (let j = 0; j < height / boxSize; j++) {
-                const x = i * boxSize;
-                const y = j * boxSize;
-                if (map.has([x, y])) {
-                    context.fillRect(x, y, boxSize, boxSize);
+        for (let i = -5; i < width / boxSize + 5; i++) {
+            for (let j = -5; j < height / boxSize + 5; j++) {
+                let xCoord = i - (x > 0 ? Math.floor(x / boxSize) : Math.ceil(x / boxSize));
+                let yCoord = j - (y > 0 ? Math.floor(y / boxSize) : Math.ceil(y / boxSize));
+                const xPos = i * boxSize + (x % boxSize);
+                const yPos = j * boxSize + (y % boxSize);
+
+                if (map[JSON.stringify([xCoord, yCoord])]) {
+                    context.fillStyle = 'black';
+                    context.fillRect(xPos, yPos, boxSize, boxSize);
                 } else {
-                    context.strokeRect(x, y, boxSize, boxSize);
+                    context.fillStyle = 'white';
+                    context.strokeRect(xPos, yPos, boxSize, boxSize);
                 }
+
+                // draw the coords
+                context.fillStyle = map[JSON.stringify([xCoord, yCoord])] ? 'white' : 'black';
+                context.fillText(`${xCoord}, ${yCoord}`, xPos + 5, yPos + 15);
             }
         }
     };
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+<svelte:window
+    bind:innerWidth={width}
+    bind:innerHeight={height}
+    on:mousedown={() => {
+        mouseDown = true;
+    }}
+    on:mousemove={({ movementX, movementY }) => {
+        if (mouseDown) {
+            dragging = true;
+            x += movementX;
+            y += movementY;
+        }
+    }}
+    on:mouseup={({ offsetX, offsetY }) => {
+        mouseDown = false;
+        if (!dragging) {
+            const [xCoord, yCoord] = screenToMap(offsetX, offsetY);
+            map[JSON.stringify([xCoord, yCoord])] = !map[JSON.stringify([xCoord, yCoord])];
+        }
+        dragging = false;
+    }}
+/>
 
 <Canvas {width} {height}>
     <Layer {render} />
